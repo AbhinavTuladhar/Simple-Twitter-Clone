6ebd6c7d432d558efb37a6ca559241e3964b6851
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-05-02 11:06:07 +0545

    Changed an issue with the add post button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,27 +2,25 @@ import { React, useState } from 'react'
 
 const Form = ({ addContent }) => {
   const [text, setText] = useState('')
-  const [buttonColour, setButtonColour] = useState('bg-blue-200')
   const [charactersRemaining, setCharactersRemaining] = useState(250)
+
+  const disabledColour = 'bg-blue-200'
+  const enabledColour = 'bg-blue-400'
 
   const handleUpdates = (event) => {
     const newText = event.target.value
     setText(newText)
-    setButtonColour(newText === '' ? 'bg-blue-200' : 'bg-blue-400')
     setCharactersRemaining(prevState => {
       return 250 - newText.length
     })
   }
 
   const addTweet = () => {
-    setText(prevState => {
-      return ''
-    })
+    setText('')
     addContent(text)
     setCharactersRemaining(250)
   }
 
-  const isDisabled = text === '' ? true : false
 
   return (
     <>
@@ -34,9 +32,15 @@ const Form = ({ addContent }) => {
             placeholder='Tweet about 30 days of React... '
             value={text}  
           />
-          <p className='items-end text-blue-400 text-right'> {charactersRemaining} </p>
+          <p 
+            className='items-end text-blue-400 text-right'
+          > {charactersRemaining} </p>
         </div>
-        <button className={`${buttonColour} text-white text-xl p-2 rounded-lg h-1/2`} disabled={isDisabled} onClick={addTweet}> Add Post </button>
+        <button 
+          className={`${text === '' ? disabledColour : enabledColour} text-white text-xl p-2 rounded-lg h-1/2`} 
+          disabled={text === '' ? true : false} 
+          onClick={addTweet}
+        > Add Post </button>
       </div>
     </>
   )
